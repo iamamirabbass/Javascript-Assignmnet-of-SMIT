8944b726ed205fd4f86b88bb5cfb12fe2f2fcee0
AMIR ABBASS <amirabbass334@gmail.com>
2024-10-24 19:22:04 +0500

    Complete till Q no 12

---
+++ Assignment-5/chap21-25.js
@@ -0,0 +1,99 @@
+// *****************************************************Qno1*****************************************************
+
+// var first_name=prompt("Enter First Name");
+// var last_name=prompt("Enter Last Name");
+
+// var user_name=first_name+last_name;
+// document.write(`Your Name is    ${user_name}!`)
+
+
+// *****************************************************Qno2*****************************************************
+// var user_favourite_mobile=prompt("Enter Your Favourite Mobile Phone!");//take input
+
+// var mobile_lenght=user_favourite_mobile.length;//make a mobile lenght variable 
+
+// document.write(`Your Favourite Mobile Phone is ${user_favourite_mobile} <br> 
+
+// Lenght of String is ${mobile_lenght}
+// `)//print 
+
+
+// *****************************************************Qno3*****************************************************
+
+// var counry_name="Pakistan";
+
+// var finding_n=counry_name.indexOf("n");
+// alert(finding_n);
+
+
+
+// *****************************************************Qno4*****************************************************
+// var word="Hello World";
+// var finding_l=word.lastIndexOf("l");
+// alert(finding_l);
+
+
+
+// *****************************************************Qno5*****************************************************
+
+// var country_name="Pakistan";
+// var find_word=country_name.charAt(3);
+// alert(find_word);
+
+
+// *****************************************************Qno6*****************************************************
+
+
+// var user_first_name=prompt("Enter Your First Name");
+// var user_last_name=prompt("Enter Your Second  Name");
+
+// var user_full_name=user_first_name.concat(user_last_name);
+// alert(`Your Name is  ${" "+user_full_name}!`);
+
+// *****************************************************Qno7*****************************************************
+
+
+// var city_1="Hyderabad";
+// var city_2=city_1.replace("Hyder","Islam");
+// alert(city_2);
+
+
+// *****************************************************Qno8*****************************************************
+
+// var message= "Ali and Sami are best friends. They play cricket and football together.";
+// var updted_message= message.replaceAll("and","&");
+
+// alert(updted_message);
+
+// *****************************************************Qno9*****************************************************
+
+// var num="472";
+// var updted_num=parseInt(num);
+// document.write(`Value ${num} <br> 
+// Type ${typeof(num)}<br>
+// value ${num}<br>
+// Type ${typeof(updted_num)}
+// `)
+
+
+// *****************************************************Qno10*****************************************************
+
+// var user_input=prompt("Enter The word You Want to convert into upper case");
+// var updted_word=user_input.toUpperCase();
+// alert(updted_word);
+
+
+// *****************************************************Qno11*****************************************************
+// var user_input=prompt("Enter The word You Want to convert into Tittle case");
+// var first_letter=user_input[0].toUpperCase();
+// var last=user_input.slice(1).toLowerCase();
+// updted_word=first_letter+last;
+// alert(updted_word);
+
+
+// *****************************************************Qno12*****************************************************
+var  num=35.36;
+alert(typeof(num));// skip
+
+
+// *****************************************************Qno13*****************************************************
