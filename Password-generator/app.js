79670023214c1d7364294a998894ef1959c12f4f
AMIR ABBASS <amirabbass334@gmail.com>
2024-11-03 16:29:40 +0500

    Random password generator
    Random password generator  complete using Html, CSS, Bootstrap and Java script

--- Password-generator/app.js
+++ Password-generator/app.js
@@ -1,9 +1,17 @@
-// var random_values="qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()QWERTYUIOPASDFGHJKLZXCVBNM";
-// var random_pass='';
-// for(var i=0;i<10;i++){
-//     var randomnum=Math.floor(Math.random()*random_values.length);
-//     random_pass+=random_values[randomnum]
-// }
-// document.write(random_pass);
+function generate_password(){
+
+    var random_values="qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()QWERTYUIOPASDFGHJKLZXCVBNM";
+var random_pass='';
+for(var i=0;i<10;i++){
+    var randomnum=Math.floor(Math.random()*random_values.length);
+    random_pass+=random_values[randomnum]
+}
+
+document.getElementById('passValue').innerHTML = `<span> ${random_Pass}  </span>`
+
+}
+
+
+
 
 
