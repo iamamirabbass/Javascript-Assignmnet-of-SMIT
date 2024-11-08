fd49efe37e6cbd900920cf703024d2e012298341
AMIR ABBASS <amirabbass334@gmail.com>
2024-11-08 20:03:01 +0500

    login page

--- Assignment-5/chap21-25.js
+++ Assignment-5/chap21-25.js
@@ -1,186 +1,238 @@
-// *****************************************************Qno1*****************************************************
+// // *****************************************************Qno1*****************************************************
 
-var first_name=prompt("Enter First Name");
-var last_name=prompt("Enter Last Name");
+// var first_name=prompt("Enter First Name");
+// var last_name=prompt("Enter Last Name");
 
-var user_name=first_name+last_name;
-document.write(`Your Name is    ${user_name}!`)
-
-
-// *****************************************************Qno2*****************************************************
-var user_favourite_mobile=prompt("Enter Your Favourite Mobile Phone!");//take input
-
-var mobile_lenght=user_favourite_mobile.length;//make a mobile lenght variable 
-
-document.write(`Your Favourite Mobile Phone is ${user_favourite_mobile} <br> 
-
-Lenght of String is ${mobile_lenght}
-`)//print 
+// var user_name=first_name+last_name;
+// document.write(`Your Name is    ${user_name}!`)
 
 
-// *****************************************************Qno3*****************************************************
+// // *****************************************************Qno2*****************************************************
+// var user_favourite_mobile=prompt("Enter Your Favourite Mobile Phone!");//take input
 
-var counry_name="Pakistan";
+// var mobile_lenght=user_favourite_mobile.length;//make a mobile lenght variable 
 
-var finding_n=counry_name.indexOf("n");
-alert(finding_n);
+// document.write(`Your Favourite Mobile Phone is ${user_favourite_mobile} <br> 
+
+// Lenght of String is ${mobile_lenght}
+// `)//print 
 
 
+// // *****************************************************Qno3*****************************************************
 
-// *****************************************************Qno4*****************************************************
-var word="Hello World";
-var finding_l=word.lastIndexOf("l");
-alert(finding_l);
+// var counry_name="Pakistan";
+
+// var finding_n=counry_name.indexOf("n");
+// alert(finding_n);
 
 
 
-// *****************************************************Qno5*****************************************************
-
-var country_name="Pakistan";
-var find_word=country_name.charAt(3);
-alert(find_word);
-
-
-// *****************************************************Qno6*****************************************************
+// // *****************************************************Qno4*****************************************************
+// var word="Hello World";
+// var finding_l=word.lastIndexOf("l");
+// alert(finding_l);
 
 
-var user_first_name=prompt("Enter Your First Name");
-var user_last_name=prompt("Enter Your Second  Name");
 
-var user_full_name=user_first_name.concat(user_last_name);
-alert(`Your Name is  ${" "+user_full_name}!`);
+// // *****************************************************Qno5*****************************************************
 
-// *****************************************************Qno7*****************************************************
-
-
-var city_1="Hyderabad";
-var city_2=city_1.replace("Hyder","Islam");
-alert(city_2);
+// var country_name="Pakistan";
+// var find_word=country_name.charAt(3);
+// alert(find_word);
 
 
-// *****************************************************Qno8*****************************************************
-
-var message= "Ali and Sami are best friends. They play cricket and football together.";
-var updted_message= message.replaceAll("and","&");
-
-alert(updted_message);
-
-// *****************************************************Qno9*****************************************************
-
-var num="472";
-var updted_num=parseInt(num);
-document.write(`Value ${num} <br> 
-Type ${typeof(num)}<br>
-value ${num}<br>
-Type ${typeof(updted_num)}
-`)
+// // *****************************************************Qno6*****************************************************
 
 
-// *****************************************************Qno10*****************************************************
+// var user_first_name=prompt("Enter Your First Name");
+// var user_last_name=prompt("Enter Your Second  Name");
 
-var user_input=prompt("Enter The word You Want to convert into upper case");
-var updted_word=user_input.toUpperCase();
-alert(updted_word);
+// var user_full_name=user_first_name.concat(user_last_name);
+// alert(`Your Name is  ${" "+user_full_name}!`);
+
+// // *****************************************************Qno7*****************************************************
+
+
+// var city_1="Hyderabad";
+// var city_2=city_1.replace("Hyder","Islam");
+// alert(city_2);
+
+
+// // *****************************************************Qno8*****************************************************
+
+// var message= "Ali and Sami are best friends. They play cricket and football together.";
+// var updted_message= message.replaceAll("and","&");
+
+// alert(updted_message);
+
+// // *****************************************************Qno9*****************************************************
+
+// var num="472";
+// var updted_num=parseInt(num);
+// document.write(`Value ${num} <br> 
+// Type ${typeof(num)}<br>
+// value ${num}<br>
+// Type ${typeof(updted_num)}
+// `)
+
+
+// // *****************************************************Qno10*****************************************************
+
+// var user_input=prompt("Enter The word You Want to convert into upper case");
+// var updted_word=user_input.toUpperCase();
+// alert(updted_word);
+
+
+// // *****************************************************Qno11*****************************************************
+// var user_input=prompt("Enter The word You Want to convert into Tittle case");
+// var first_letter=user_input[0].toUpperCase();
+// var last=user_input.slice(1).toLowerCase();
+// updted_word=first_letter+last;
+// alert(updted_word);
+
+
+// // *****************************************************Qno12*****************************************************
+// var  num=35.36;
+// document.write(typeof(num));
+// var num_type=num.toString();
+// document.write(typeof(num_type));
+// var updated_num = num_type.replace(".","");
+// document.write(updated_num);
+
+// // *****************************************************Qno13*****************************************************
+// var user_name=prompt("Enter User Name");
+// var incorrect=false;
+// for (var i=0;i<=user_name.length;i++){
+// if (user_name.charCodeAt()===33||user_name.charCodeAt()==44||user_name.charCodeAt()==46||user_name.charCodeAt()==64)
+//     {incorrect=true;
+// }
+
+// }
+// if (incorrect){
+// alert("Please Enter Valid Password");
+
+// }
+// else{
+// alert("Password Sucessfully Generated!")
+
+// }
+
+// // *****************************************************Qno14*****************************************************
+// var bakery=["cake","apple pie","cookie","chips","patties"];
+// var user_item=prompt("What do you want To Order?")
+// var foundit=false;
+// var index;
+// for(var i=0;i<=bakery.length;i++){
+//     if(user_item.toLowerCase()===bakery[i]){
+//         foundit=true;
+//         index=i;
+//     }
+// }
+// if (foundit){
+//     alert(user_item+" Available at Index "+index)
+// }
+// else{
+//     alert(user_item+" not available")
+// }
+
+// // *****************************************************Qno15*****************************************************
+
+// var user_pass=prompt("Enter Password");
+// var correct_pass=false;
+
+// for (var i=0;i<=user_pass.length;i++){
+// if ((user_pass.charCodeAt() >=48&&user_pass.charCodeAt()<=57)&&(user_pass.charCodeAt()>=65&&user_pass.charCodeAt()<=90||user_pass.charCodeAt()>=97&&user_pass.charCodeAt()<=122)&&(user_pass.charCodeAt()[0]!=48&&user_pass.charCodeAt()<=57)&&user_pass.length>=6){
+//     correct_pass=true;
+// }
+// }
+// if(correct_pass){
+//     alert("Password Sucessfully Generated!")
+// }
+// else{
+//     alert("Please Enter Valid Password!")
+// }
+
+
+
+// // *****************************************************Qno16*****************************************************
+
+// var university="University of karachi";
+// var updated_uni=university.split('');
+// for (var i=0;i<updated_uni.length;i++){
+//     document.write(updated_uni[i]+"<br>");
+// }
+
+
+// // *****************************************************Qno17*****************************************************
+// var user_name=prompt("Enter The Word");
+// var last_word=user_name.length-1;
+// var output=user_name[last_word];
+// alert(output); 
+
+
+
+// // *****************************************************Qno18*****************************************************
+
+// var text= "the quick brown fox jumps over the lazy dog";    
+
+// var word_count=[];
+// for(var i=0;i<text.length;i++){
+//     if(text[i]==="t"){
+//         word_count[++1];
+//     }
+
+// }
+
+// alert(word_count)
+
 
 
 // *****************************************************Qno11*****************************************************
-var user_input=prompt("Enter The word You Want to convert into Tittle case");
-var first_letter=user_input[0].toUpperCase();
-var last=user_input.slice(1).toLowerCase();
-updted_word=first_letter+last;
-alert(updted_word);
+
+// var current_time = new Date();
+// var hour_ago = new Date(current_time.setHours(current_time.getHours() - 1))
+// document.write(`Current Date: ${new Date()}<br>
+
+//     1 hour ago it was: ${hour_ago}
+
+//     `)
+
 
 
 // *****************************************************Qno12*****************************************************
-var  num=35.36;
-document.write(typeof(num));
-var num_type=num.toString();
-document.write(typeof(num_type));
-var updated_num = num_type.replace(".","");
-document.write(updated_num);
+
+// var current_Date = new Date();
+// alert(current_Date);
+// var back_100yrs = new Date(current_Date.setFullYear(current_Date.getFullYear() - 100))
+// document.write(back_100yrs)
+
+
 
 // *****************************************************Qno13*****************************************************
-var user_name=prompt("Enter User Name");
-var incorrect=false;
-for (var i=0;i<=user_name.length;i++){
-if (user_name.charCodeAt()===33||user_name.charCodeAt()==44||user_name.charCodeAt()==46||user_name.charCodeAt()==64)
-    {incorrect=true;
-}
 
-}
-if (incorrect){
-alert("Please Enter Valid Password");
-
-}
-else{
-alert("Password Sucessfully Generated!")
-
-}
-
-// *****************************************************Qno14*****************************************************
-var bakery=["cake","apple pie","cookie","chips","patties"];
-var user_item=prompt("What do you want To Order?")
-var foundit=false;
-var index;
-for(var i=0;i<=bakery.length;i++){
-    if(user_item.toLowerCase()===bakery[i]){
-        foundit=true;
-        index=i;
-    }
-}
-if (foundit){
-    alert(user_item+" Available at Index "+index)
-}
-else{
-    alert(user_item+" not available")
-}
-
-// *****************************************************Qno15*****************************************************
-
-var user_pass=prompt("Enter Password");
-var correct_pass=false;
-
-for (var i=0;i<=user_pass.length;i++){
-if ((user_pass.charCodeAt() >=48&&user_pass.charCodeAt()<=57)&&(user_pass.charCodeAt()>=65&&user_pass.charCodeAt()<=90||user_pass.charCodeAt()>=97&&user_pass.charCodeAt()<=122)&&(user_pass.charCodeAt()[0]!=48&&user_pass.charCodeAt()<=57)&&user_pass.length>=6){
-    correct_pass=true;
-}
-}
-if(correct_pass){
-    alert("Password Sucessfully Generated!")
-}
-else{
-    alert("Please Enter Valid Password!")
-}
+// var user_dob=prompt("Enter Your Birth Year");
+// var user_age=new Date().getFullYear()-user_dob;
+// document.write(`Your Age IS: ${user_age}` )
 
 
+// // *****************************************************Qno14*****************************************************
 
-// *****************************************************Qno16*****************************************************
-
-var university="University of karachi";
-var updated_uni=university.split('');
-for (var i=0;i<updated_uni.length;i++){
-    document.write(updated_uni[i]+"<br>");
-}
-
-
-// *****************************************************Qno17*****************************************************
-var user_name=prompt("Enter The Word");
-var last_word=user_name.length-1;
-var output=user_name[last_word];
-alert(output); 
+// var customer_name=prompt("Enter Your Name");
+// var current_month=prompt("Enter Current Month");
+// var units=+prompt("Enter Number Of Units");
+// var per_unit=+prompt("Charges Per Unit");
+// var net_amount=units*per_unit;
+// var late_charges=350;
+// var late_amount=net_amount+late_charges;
 
 
+// document.write(`Customer Name: ${customer_name}<br>
+//     Month: ${current_month}<br>
+//     Number Of Units: ${units}<br>
+//     Charges Per Unit: ${per_unit}<br>
+//     <br>
+//     Net Amount Payable (within Due Date): ${net_amount}<br>
+//     Late Payment Surcharge: ${late_charges}<br>
+//     Gross Amount Payable (After Due Date): ${late_amount}<br>
 
-// *****************************************************Qno18*****************************************************
-
-var text= "the quick brown fox jumps over the lazy dog";    
-
-var word_count=[];
-for(var i=0;i<text.length;i++){
-    if(text[i]==="t"){
-        word_count[++1];
-    }
-
-}
-
-alert(word_count)
+//     `)
--- Assignment-5/chap26-30.js
+++ Assignment-5/chap26-30.js
@@ -1,77 +1,77 @@
-// *****************************************************Qno1*****************************************************
-var user_input=+prompt("Enter Positive Positive Value");
-document.write(` Value: ${user_input}<br>
+// // *****************************************************Qno1*****************************************************
+// var user_input=+prompt("Enter Positive Positive Value");
+// document.write(` Value: ${user_input}<br>
     
-    Round OF: ${Math.round(user_input)}<b
-    r>
-Floor: ${Math.floor(user_input)}<br>
-Ceil: ${Math.ceil(user_input)}<br>
+//     Round OF: ${Math.round(user_input)}<b
+//     r>
+// Floor: ${Math.floor(user_input)}<br>
+// Ceil: ${Math.ceil(user_input)}<br>
 
-`
-);
+// `
+// );
 
-// *****************************************************Qno2*****************************************************
-var user_input=+prompt("Enter Negative Integer  Value");
-document.write(` Value: ${user_input}<br>
+// // *****************************************************Qno2*****************************************************
+// var user_input=+prompt("Enter Negative Integer  Value");
+// document.write(` Value: ${user_input}<br>
     
-    Round OF: ${Math.round(user_input)}<br>
-Floor: ${Math.floor(user_input)}<br>
-Ceil: ${Math.ceil(user_input)}<br>
-`);
+//     Round OF: ${Math.round(user_input)}<br>
+// Floor: ${Math.floor(user_input)}<br>
+// Ceil: ${Math.ceil(user_input)}<br>
+// `);
 
-// *****************************************************Qno3*****************************************************
+// // *****************************************************Qno3*****************************************************
 
-var user_input=+prompt("Enter the Value");
-var absolute_value=Math.abs(user_input);
-alert(absolute_value);
-
-
-
-// *****************************************************Qno4*****************************************************
-var random=Math.floor(Math.random()*6)+1;
-var random_value=random.toFixed(0);
-document.write(`Random Dice Value is ${random_value}`);
-
-
-// *****************************************************Qno5*****************************************************
-
-var random=Math.floor(Math.random()*2)+1;
-var random_value=random.toFixed(0);
-if(random_value==1){
-    document.write(`${random_value} Random Coin Value Head`);
-
-}
-else{
-    document.write(`${random_value} Random Coin Value Tail`);
-
-}
-
-
-// *****************************************************Qno6*****************************************************
-
-var random=Math.floor(Math.random()*100)+1;
-var random_value=random.toFixed(0);
-document.write(`Random  Value is ${random_value}`);
+// var user_input=+prompt("Enter the Value");
+// var absolute_value=Math.abs(user_input);
+// alert(absolute_value);
 
 
 
-// *****************************************************Qno7*****************************************************
-
-var user_weight=prompt("Enter Your Weight In Kilograms");
-
-var result=parseFloat(user_weight);
-alert("You Weight Is "+result+"Kilogram");
+// // *****************************************************Qno4*****************************************************
+// var random=Math.floor(Math.random()*6)+1;
+// var random_value=random.toFixed(0);
+// document.write(`Random Dice Value is ${random_value}`);
 
 
+// // *****************************************************Qno5*****************************************************
+
+// var random=Math.floor(Math.random()*2)+1;
+// var random_value=random.toFixed(0);
+// if(random_value==1){
+//     document.write(`${random_value} Random Coin Value Head`);
+
+// }
+// else{
+//     document.write(`${random_value} Random Coin Value Tail`);
+
+// }
 
 
-// *****************************************************Qno8*****************************************************
+// // *****************************************************Qno6*****************************************************
 
-var seceret_num=Math.floor(Math.random()*6)+1;
-var user_input=+prompt("Enter Any Number To Check");
-if(user_input===seceret_num){
-    alert("Congratulation You are the Winner!")
-}
-else{
-    alert("Try Again!")
-}
+// var random=Math.floor(Math.random()*100)+1;
+// var random_value=random.toFixed(0);
+// document.write(`Random  Value is ${random_value}`);
+
+
+
+// // *****************************************************Qno7*****************************************************
+
+// var user_weight=prompt("Enter Your Weight In Kilograms");
+
+// var result=parseFloat(user_weight);
+// alert("You Weight Is "+result+"Kilogram");
+
+
+
+
+// // *****************************************************Qno8*****************************************************
+
+// var seceret_num=Math.floor(Math.random()*6)+1;
+// var user_input=+prompt("Enter Any Number To Check");
+// if(user_input===seceret_num){
+//     alert("Congratulation You are the Winner!")
+// }
+// else{
+//     alert("Try Again!")
+// }
