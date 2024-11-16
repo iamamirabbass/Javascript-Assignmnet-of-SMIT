d8c970323e131de5355db62b537598857c141466
AMIR ABBASS <amirabbass334@gmail.com>
2024-11-16 16:16:46 +0500

    JS added

--- Head&Tail Game/app.js
+++ Head&Tail Game/app.js
@@ -1,5 +1,14 @@
-var user_1name=prompt("Enter The Name of First Player")
-var user_2name=prompt("Enter The Name of Second Player")
+// var user_1name=prompt("Enter The Name of First Player")
+// var user_2name=prompt("Enter The Name of Second Player")
 
 
+function player_info (){
+    var player1name=document.getElementById('player_1')
+    var player2name=document.getElementById('player_2')
+    selectiontext.innerHTML=player1name.value+" To Choose";       
+}
 
+function head(){
+    decidetext.innerHTML=player1name.value+"Choose Head and "+player2name+" Choose Tail";
+    
+}
