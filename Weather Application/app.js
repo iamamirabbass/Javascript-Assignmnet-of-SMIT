2ab88f2f60bb20ef26d4f8c3072999840000f424
AMIR ABBASS <amirabbass334@gmail.com>
2025-01-05 15:32:24 +0500

    Update app.js

--- Weather Application/app.js
+++ Weather Application/app.js
@@ -1,28 +1,8 @@
-// var latitude=67.0598332217543;
-// var longitude=24.953709678545295; 
-// const url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=679cddd303fcba19fd40073024f2f1df`;
-// const cityname="Karachi"
-// const searchbycity=`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=679cddd303fcba19fd40073024f2f1df`
-
 let cityInput = document.getElementById('cityInp')
 let temperature = document.getElementById('temp')
 let windSpeed = document.getElementById('windSpeed')
 let feelsLike = document.getElementById('feelsLike')
 let placeLocation = document.getElementById('loc')
-
-
-async function getData() {
-    try {
-      await fetch(url)
-        .then((data) => data.json())
-        .then((res) => console.log(res))
-        .catch((error) => console.log(error));
-    } catch (error) {}
-  }
-  setTimeout(() => {
-          getData()
-  }, 2000); 
-
 
 
   async function checkWeather(){
@@ -54,4 +34,3 @@ async function getData() {
   feelsLike.innerText = `${Math.floor(weatherData.main.feels_like - 273.15)}\u00B0C`
 
 }
-
